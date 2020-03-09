b652ab7e0834685ab279bbb05c08b185365e19df
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-08 19:22:40 -0700

    Add project files.

---
+++ Assignment 160/MathFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_160
+{
+    class MathFunction
+    {
+        public int twoInt(int int1, int int2 = 0)
+        {
+            int Total;
+            Total = int1 * 5 + int2;
+            return Total;
+        }
+    }
+}
---
+++ Assignment 160/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1;
+            string num2;
+            int int2;
+            int Total;
+            MathFunction functionOne = new MathFunction();
+
+            Console.WriteLine("Enter a number:");
+            int1 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number:(optional)");
+            num2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(num2))
+            {
+                num2 = "0";
+                int2 = Convert.ToInt32(num2);
+                Total = functionOne.twoInt(int1, int2);
+
+                Console.WriteLine(int1 + " times 5 equals:");
+                Console.WriteLine(Total);
+            } else
+            {
+                int2 = Convert.ToInt32(num2);
+                Total = functionOne.twoInt(int1, int2);
+
+                Console.WriteLine(int1 + " times 5, plus " + int2 + " equals:");
+                Console.WriteLine(Total);
+            }          
+            Console.ReadLine();
+              
+        }
+    }
+}
